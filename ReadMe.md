536d0e069bca8247ca82a3cb7ad6a1bd28da5161
TheDeathDragon <249501281@qq.com>
2024-07-17 17:59:12 +0800

    Init

---
+++ ReadMe.md
@@ -0,0 +1,21 @@
+## 说明
+
+预置进去如果要修改默认布局，请参考以下修改：
+
+把谷歌默认的 `widget` 替换
+
+`vendor/partner_gms/apps/GmsSampleIntegration/res_dhs_full/xml/partner_default_layout.xml`
+
+```xml
+<appwidget
+screen="0"
+   x="0"
+   y="1"
+   spanX="4"
+   spanY="1"
+   packageName="la.shiro.widget"
+   className="la.shiro.widget.DigitalClockWidget"/> 
+```
+
+
+
---
+++ app/src/main/java/la/shiro/widget/DigitalClockWidget.kt
@@ -0,0 +1,19 @@
+package la.shiro.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+class DigitalClockWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.widget_digital_clock)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
